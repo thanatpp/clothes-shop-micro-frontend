76972b86986369fdd29e0b1344645ea6edb91baf
Thanat <72113322+Thanatpp@users.noreply.github.com>
2021-08-05 14:16:22 +0700

    create women item component

---
+++ women-item/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
