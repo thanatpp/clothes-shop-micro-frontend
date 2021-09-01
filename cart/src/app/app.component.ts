d99a9909ce4c3ecc42e4ac9178e9b77788719e31
Thanat <72113322+Thanatpp@users.noreply.github.com>
2021-09-01 19:06:18 +0700

    design cart item && function

--- cart/src/app/app.component.ts
+++ cart/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cart-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cart';
+export class AppComponent implements OnInit {
+
+  items: any[]
+
+  ngOnInit(){
+    //fake items
+    this.items = [{name: "THE GODFAGLE TEE", price: 320, amount:1}, {name: "THE GODFAGLE TEE", price: 790, amount:2}, {name: "THE GODFAGLE TEE", price: 199, amount:1}, 
+                  {name: "THE GODFAGLE TEE", price: 690, amount:1}]
+  }
+
+  amountChange(e, i){
+    this.items[i].amount = e.target.value
+    console.log(this.items)
+  }
+
+  minus(i){
+    if(this.items[i].amount != 1){
+      this.items[i].amount = Number(this.items[i].amount) - 1
+    }
+    console.log(this.items)
+  }
+
+  plus(i){
+    this.items[i].amount = Number(this.items[i].amount) + 1
+    console.log(this.items)
+  }
+
+  price(i: any){
+  
+    return Intl.NumberFormat().format(this.items[i].amount * this.items[i].price)
+  }
+
+  totalPrice(){
+    var total = 0
+    for(var i in this.items){
+      total += this.items[i].amount * this.items[i].price
+    }
+    return Intl.NumberFormat().format(total)
+  }
+
+  totalAmount(){
+    var amount = 0
+    for(var i in this.items){
+      amount += this.items[i].amount
+    }
+    return Intl.NumberFormat().format(amount)
+  }
+  
 }
--- cart/src/app/app.module.ts
+++ cart/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumberDirective } from './number-only.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    EmptyRouteComponent,
+    NumberDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
