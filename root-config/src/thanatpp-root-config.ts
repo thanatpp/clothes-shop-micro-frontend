c9ae4b1f97651ab3b3280f0ae2dc3e6045672389
Thanat <72113322+Thanatpp@users.noreply.github.com>
2021-09-17 20:57:14 +0700

    simple Auth

--- root-config/src/thanatpp-root-config.ts
+++ root-config/src/thanatpp-root-config.ts
@@ -1,4 +1,4 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start, navigateToUrl } from "single-spa";
 import {
   constructApplications,
   constructRoutes,
@@ -6,15 +6,44 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
+window.addEventListener('single-spa:before-routing-event', (e: CustomEvent) => {
+  const {
+    originalEvent,
+    newAppStatuses,
+    appsByNewStatus,
+    totalAppChanges,
+    oldUrl,
+    newUrl,
+    navigationIsCanceled,
+    cancelNavigation,
+  } = e.detail;
+
+  if(localStorage.getItem('token') === null){
+    if(new URL(newUrl).pathname  === '/account/profile'){
+      navigateToUrl('/login')
+    }
+  }else if(localStorage.getItem('token') !== null){
+    if(new URL(newUrl).pathname  === '/login'){
+      navigateToUrl('/')
+    }
+  }
+});
+
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
-    return System.import(name);
+    return System.import(name)          
   },
 });
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
-start();
+
+start({
+  urlRerouteOnly: true,
+});
+
+
+
